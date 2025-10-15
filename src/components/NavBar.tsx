dd483f00277c8f1c53db1d19c3c32851f1250e0f
SergioGMunoz <sergiogmunozdev@gmail.com>
2025-10-16 00:01:30 +0200

    feat: refactor color management and replace ColorSwitcher with NavBar
    - Added new dependencies for MUI icons and Emotion styling.
    - Removed ColorSwitcher component and integrated color changing functionality into NavBar.
    - Implemented ColorCycler class to manage accent colors with localStorage support.
    - Updated useAccentColor hook to utilize ColorCycler for accent color management.
    - Created FloatingDock component for a floating navigation bar with smooth scrolling and color changing features.

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,59 @@
+import { FloatingDock } from "@/components/ui/floating-dock";
+import HomeIcon from "@mui/icons-material/Home";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import ColorLensIcon from "@mui/icons-material/ColorLens";
+import { useAccentColor } from "@/hooks/useAccentColor";
+import { useEffect, useState } from "react";
+
+const NavBar = () => {
+  // Scrolling to sections
+  const scrollToSection = (id: string) => {
+    const element = document.querySelector(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  // Dark Mode
+  const handleDarkMode = () => {
+    console.log("darkmode");
+  };
+
+  // Color Changing
+  const { cycleToNextColor, getNextColor } = useAccentColor();
+  const [nextColor, setNextColor] = useState(getNextColor());
+  const handleAccentColor = () => {
+    cycleToNextColor();
+    setNextColor(getNextColor());
+  };
+
+  const links = [
+    {
+      title: "Home",
+      icon: <HomeIcon className="text-accent" />,
+      onClick: () => scrollToSection("#home"),
+    },
+    {
+      title: "Modo Oscuro",
+      icon: <DarkModeIcon className="text-accent" />,
+      onClick: handleDarkMode,
+    },
+    {
+      title: `Cambiar Color`,
+      icon: <ColorLensIcon style={{ color: nextColor.value }} />,
+      onClick: handleAccentColor,
+    },
+  ];
+
+  return (
+    <div className="flex items-center justify-center h-[35rem] w-full">
+      <FloatingDock
+        items={links}
+        className="fixed bottom-10 left-1/2 transform -translate-x-1/2 z-50"
+      />
+    </div>
+  );
+};
+
+export default NavBar;
