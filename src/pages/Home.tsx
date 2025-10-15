9fe8d2733153c3b65e549e68968fffb976c52151
SergioGMunoz <sergiogmunozdev@gmail.com>
2025-10-15 14:07:12 +0200

    feat: add About component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
+import About from '@/components/About';
 import ColorSwitcher from '@/components/ColorSwitcher';
 import Title from '@/components/Title';
 
 const Home = () =>{
     return(
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center relative">
+        <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center gap-4 relative">
             <ColorSwitcher />
             <Title/>
+            <About/>
         </div>
     );
 }
