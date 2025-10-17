90975d32f662a6dcd299700492f35306411d199f
SergioGMunoz <sergiogmunozdev@gmail.com>
2025-10-17 14:07:40 +0200

    fix: correct spelling of "projects" in localization files and update description key in projectsData

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import CardTemplate from "./CardTemplate";
+import { useTranslation } from "react-i18next";
 
 interface ProjectDataInterface {
   projectData: {
@@ -13,11 +14,13 @@ interface ProjectDataInterface {
 }
 
 const ProjectCard = ({ projectData }: ProjectDataInterface) => {
-    return (
+  const { t } = useTranslation();
+
+  return (
     //   <h1>{projectData.titleKey}</h1>
     //   <img ></img>
     <CardTemplate>
-        <button
+      <button
         type="button"
         className="my-10 flex w-80 cursor-pointer flex-col items-stretch rounded-[16px] border-0 bg-[#1F2121] p-2 saturate-0 md:my-20 md:p-4"
         aria-label="View invite F7RA"
@@ -27,22 +30,27 @@ const ProjectCard = ({ projectData }: ProjectDataInterface) => {
           opacity: 1,
         }}
       >
+        {/* Title */}
+        <div className="mt-2 flex flex-shrink-0 items-center justify-between p-4 font-mono text-white">
+          <div className="text-xs">{t(projectData.titleKey)}</div>
+          <div className="text-xs text-gray-300 opacity-50">
+            {t(projectData.descriptionKey)}
+          </div>
+        </div>
+        {/* IMG */}
         <div className="mx-2 flex-1">
           <div className="relative mt-2 aspect-video w-full">
             <img
               loading="lazy"
               className="absolute inset-0 h-full w-full rounded-[16px] bg-[#000000] object-cover contrast-75"
-              src={projectData.picture} alt={projectData.titleKey}
+              src={projectData.picture}
+              alt={projectData.titleKey}
               style={{
                 boxShadow: "rgba(0, 0, 0, 0.05) 0px 5px 6px 0px",
                 opacity: 1,
               }}
             />
           </div>
-        </div>
-        <div className="mt-2 flex flex-shrink-0 items-center justify-between p-4 font-mono text-white">
-          <div className="text-xs">Comet Invitation</div>
-          <div className="text-xs text-gray-300 opacity-50">#F7RA</div>
         </div>
       </button>
     </CardTemplate>
