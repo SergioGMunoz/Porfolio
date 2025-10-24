741c8f5d8498069df58b6eca5a80132da2d76447
SergioGMunoz <sergiogmunozdev@gmail.com>
2025-10-24 20:25:50 +0200

    fix: update margin for project title in Projects component for improved spacing

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,7 +10,7 @@ const Projects = () => {
     <div>
       {/* Title Projects */}
       <h3
-        className="text-3xl font-bold text-center mb-2"
+        className="text-3xl font-bold text-center mb-12"
         style={{
           color: "var(--color-accent)",
         }}
