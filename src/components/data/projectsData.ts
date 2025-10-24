3d4249e3dcea68302c59fc596aad32936cb8fe1e
SergioGMunoz <sergiogmunozdev@gmail.com>
2025-10-24 19:59:35 +0200

    feat: add Badge component and refactor project stack to use tech objects

--- src/components/data/projectsData.ts
+++ src/components/data/projectsData.ts
@@ -1,13 +1,24 @@
+const tech = {
+  react: {
+    text: "react",
+    color: "#61DAFB",
+  },
+  javascript: {
+    text: "javascript",
+    color: "#F7DF1E",
+  },
+};
+
 const projectsData = [
   {
     id: 1,
     titleKey: "projects.MurciaH&R.title",
     descriptionKey: "projects.MurciaH&R.description",
-    stack: ["React", "JavaScript", "API Rest"],
+    stack: [tech.react, tech.javascript],
     picture: "/projects/MurciaH&R.png",
     link: "https://github.com/SergioGMunoz/Murcia-H-R",
     readmeLink: `https://raw.githubusercontent.com/SergioGMunoz/Murcia-H-R/main/README.md`,
-  }
+  },
 ];
 
 export default projectsData;
---
+++ src/components/projects/Badge.tsx
@@ -0,0 +1,22 @@
+interface BadgeProps {
+    color: string;
+    text: string;
+}
+
+const Badge = ({ color, text }: BadgeProps) => {
+    const bgStyle = {
+        backgroundColor: color ? `${color}4D` : 'rgba(0,0,0,0.05)', 
+    };
+    return (
+        <div
+            className="inline-block rounded-xl px-3 py-1"
+            style={bgStyle}
+        >
+            <p className="text-black text-xs font-semibold">
+                {text}
+            </p>
+        </div>
+    );
+}
+
+export default Badge;
